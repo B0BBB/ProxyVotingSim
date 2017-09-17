984ed8caf5745b7bb666427cb067a788961fec46
Oleg <olegzendel@campus.technion.ac.il>
2017-09-17 15:11:41 +0300

    Add output to CSV

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from time import time
 from collections import defaultdict
 from random import sample
+import csv
 # Library located at https://pypi.python.org/pypi/Distance/
 from distance import hamming
 import VotingAgent
@@ -13,18 +14,28 @@ def main():
     create_mel_dist(Truth, PHI, hamming, K)
     data = create_f_pop(PopSize, Mel)
     distanceTable = defaultdict(list)
-    for run in range(Runs):
-        activeAgents = sample(data, N)
-        print 'Simulation number ', run + 1
-        for scenario in Scenario:
-            reset_active_agents(data, scenario)
-            ballots = create_ballots(data, activeAgents, scenario)
-            result = bm_majority_vote(ballots)
-            distanceTable[scenario] += [hamming(result, Truth)]
-            # print 'Scenario ', scenario, 'result :', hamming(result, Truth)
-    for scenario in Scenario:
-        print 'the average distance for ', scenario, 'is ', float(sum(distanceTable[scenario])) / float(
-            len(distanceTable[scenario]))
+    counter = 1
+    for n in range(1, N + 1):
+        for run in range(Runs):
+            activeAgents = sample(data, n)
+            print 'Simulation number ', counter
+            for scenario in Scenario:
+                reset_active_agents(data, scenario)
+                ballots = create_ballots(data, activeAgents, scenario)
+                result = bm_majority_vote(ballots)
+                distanceTable[(scenario, n)] += [hamming(result, Truth)]
+            counter += 1
+                # print 'Scenario ', scenario, 'result :', hamming(result, Truth)
+    # for scenario, n in distanceTable:
+    #     print 'the average distance for ', scenario, 'with', n, 'agents' , 'is ', float(sum(distanceTable[(scenario, n)])) / float(len(distanceTable[(scenario, n)]))
+
+    with open('Test1.csv', 'w') as write_file:
+        writer = csv.writer(write_file, lineterminator='\n')
+        fieldnames2 = ["Scenario", "Agents", "Avg dist"]
+        writer.writerow(fieldnames2)
+        for s, n in distanceTable:
+            writer.writerow([s, n, float(sum(distanceTable[(s, n)])) / float(len(distanceTable[(s, n)]))])
+    write_file.close()
 
 
 if __name__ == '__main__':
